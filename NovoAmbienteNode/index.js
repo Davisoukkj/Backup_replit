dac8567edf7b3e8ff12e583d399600f3154a9dc8
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2024-10-18 07:52:36 -0300

    Novo Ambiente Node

---
+++ NovoAmbienteNode/index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+console.dir(http)
+
+/*
+const arr = [1,5,6,8,12,32,3]
+console.table(arr)
+console. error("Error!")
+console.warn("Attempino")
+console.info(arr)
+console.time()
+console.dir(arr)
+
+console.log(arr)
+*/
