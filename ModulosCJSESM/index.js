69173468d3ab83fb70af505f929282b1db1d38a7
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2024-10-18 08:27:42 -0300

    ModulosCJSESM

---
+++ ModulosCJSESM/index.js
@@ -0,0 +1,15 @@
+//IMPORT
+import path from 'path';
+import { getGuilda } from './lib/personagem';
+import {getPersonagem} from './lib/personagem.cjs';
+import getPersonagem from './lib/personagem.cjs';
+import { http } from './lib/nativo.cjs';
+//CHAMANDO FUNÇÃO DO MOD. PERSONAGEM
+console.log(getPersonagem());
+getGuilda() //CONSOLE.LOG()
+
+//CRIANDO MODULOS CJS
+console.log(fs);
+console.log(path);
+console.log(http);
+//CHAMANDO MODULOS ESM
---
+++ ModulosCJSESM/lib/personagem.js
@@ -0,0 +1,12 @@
+//EXPORT ESM
+function getPersonagem() {
+  return {
+    nome: "goku",
+    poder: "kamehameha",
+    energia: "100",
+  };
+}
+export function getGuilda() {
+  console.log("saya hoken");
+}
+export default getPersonagem;
